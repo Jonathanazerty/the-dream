da320b5b53bf8d16a3afdd4ab5ef8312f9a8d18e
Jonathan <84382972+Jonathanazerty@users.noreply.github.com>
2021-07-28 12:30:52 +0200

    add changes php

---
+++ exchange.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>The Dream</title>
+    </head>
+    <body>
+        
+        <header>
+            <div class="title">
+                <h1>Exchange rate Calculator - Cuba</h1>
+            </div>
+        </header>
+
+        <div class="exchange-container">
+            <form action="exchange.php" method="post">
+                <label for="country"> Cuba </label>
+                <label for="query">The amount you would like to convert:</label>
+                <input type="text" id="query" name="query">
+                <input type="submit" name="submit" value="convert now">
+            </form>
+            <?php
+
+            ?>
+
+        </div>
+    </body>
+</html>
